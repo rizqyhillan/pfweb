f4cde1bc91ca3a8c9279d4a6e5f4fd278391fa95
Arthur Cimory <rizqycoe@gmail.com>
2026-05-10 14:17:30 +0700

    add sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = ['nama', 'email', 'password', 'role', 'no_hp', 'alamat', 'is_aktif'];
     protected $hidden = ['password', 'remember_token'];
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\Api\MedicalRecordController;
 use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\Api\MobileAuthController;
 
 Route::get('/test', function () {
     return response()->json([
@@ -11,10 +13,24 @@ Route::get('/test', function () {
     ]);
 });
 
-Route::get('/medical-records', [MedicalRecordController::class, 'index']);
-Route::get('/pets/{id}/medical-records', [MedicalRecordController::class, 'byPet']);
-Route::post('/medical-records', [MedicalRecordController::class, 'store']);
+Route::post('/login', [MobileAuthController::class, 'login']);
 
-Route::get('/transactions', [TransactionController::class, 'index']);
-Route::get('/transactions/{id}', [TransactionController::class, 'show']);
-Route::get('/transactions/status/{status}', [TransactionController::class, 'byStatus']);
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [MobileAuthController::class, 'logout']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Medical Records
+    Route::get('/medical-records', [MedicalRecordController::class, 'index']);
+    Route::get('/pets/{id}/medical-records', [MedicalRecordController::class, 'byPet']);
+    Route::post('/medical-records', [MedicalRecordController::class, 'store']);
+
+    // Transactions
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::get('/transactions/{id}', [TransactionController::class, 'show']);
+    Route::get('/transactions/status/{status}', [TransactionController::class, 'byStatus']);
+
+});
